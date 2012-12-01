39f7d79455aeec1951db7c0e042b778379a34341
tomaszjureczko <tomasz.jureczko@gmail.com>
2012-12-01 22:53:19 +0000

    gender done

--- js/app.js
+++ js/app.js
@@ -1,7 +1,27 @@
 $(document).ready(function(){
 
+    location.hash = "";
+    
     $('#gender img').wiggle('start', {
         wiggleDegrees: ['.5','1','.5','0','-0.5','-1','-0.5','0'],
     });
+
+    $("#gender img").hover(
+        function () {
+            $(this).wiggle('stop').wiggle('start', {
+                wiggleDegrees: ['2','4','2','0','-2','-4','-2','0'],
+            }).addClass("hover");
+        },
+        function () {
+            $(this).wiggle('stop').wiggle('start', {
+                wiggleDegrees: ['.5','1','.5','0','-0.5','-1','-0.5','0'],
+            }).removeClass("hover");
+        }
+    );
+
+    $("#gender img").click(function() {
+        location.hash = "#age";
+    });
+    
 
 });
